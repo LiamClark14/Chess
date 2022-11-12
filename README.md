2cff35fab2194d44ee374909b63ba8269ac58583
LIAM CLARK <liamclark@duke.edu>
2022-11-12 14:45:18 -0500

    Added Tile Class

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Chess
-building a chess application using object oriented principles in Java
+building a chess application using object oriented principles in Java following guide
+Software Architecture & Design on youtube
---
+++ src/Tile.java
@@ -0,0 +1,48 @@
+// abstraction for a single chess tile
+public abstract class Tile {
+    // zero to 63
+    int tileCoordinate;
+
+    Tile(int tileCoordinate){
+        this.tileCoordinate = tileCoordinate;
+    }
+
+    public abstract boolean isTileOccupied();
+
+    public abstract Piece getPiece();
+
+    public static final class EmptyTile extends Tile{
+        EmptyTile(int tileCoordinate){
+            super(tileCoordinate);
+        }
+        @Override
+        public boolean isTileOccupied(){
+            return false;
+        }
+
+        @Override
+        public Piece getPiece(){
+            return null;
+        }
+    }
+
+    public static final class OccupiedTile extends Tile {
+        Piece pieceOnTile;
+
+        OccupiedTile(int tileCoordinate, Piece pieceOnTile){
+            super(tileCoordinate);
+            this.pieceOnTile = pieceOnTile;
+        }
+
+        @Override
+        public boolean isTileOccupied(){
+            return true;
+        }
+
+        @Override
+        public Piece getPiece(){
+            return this.pieceOnTile;
+        }
+    }
+
+}
